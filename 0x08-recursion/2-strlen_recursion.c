730c104f470c239c77e727878c31e5be4981cee1
onemmanuel4christ <onemmanuel4christ@gmail.com>
2023-07-05 15:02:13 -0700

    2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,13 @@ nclude "main.h"
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
 	}
-	return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
 
