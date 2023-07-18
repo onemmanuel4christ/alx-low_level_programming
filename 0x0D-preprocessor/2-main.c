5e4ac609dc5b723d62ee55bcef7ec3306a258c84
onemmanuel4christ <onemmanuel4christ@gmail.com>
2023-07-18 04:37:40 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ * Description- a funtion to print file name in a new  lin
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
