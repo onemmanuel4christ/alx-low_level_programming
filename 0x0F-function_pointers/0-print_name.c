bf4582d013d91964b8f2e700a212e6794eea4e8d
onemmanuel4christ <onemmanuel4christ@gmail.com>
2023-07-20 16:46:48 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
-/**
- * print_name - Write a function that prints a name
- * @name: name of the person
- * @f: paramter for the function pointer
- * Return: Nothing.
- */
-void print_name(char *name, void (*f)(char *))
-{
-	(*f)(name);
-}
-}
+	#include <stdio.h>
+	/**
+	 * print_name - print name using pointer to function
+	 * @name: string to add
+	 * @f: pointer to function
+	 * Return: nothing
+	 **/
+	void print_name(char *name, void (*f)(char *))
+	{
+		if (name == NULL || f == NULL)
+			return;
+
+
+		f(name);
+	}
+
